2c825f29533da348e7eb366a06a518b29bc9cb1b
jestfer <jestfer6@gmail.com>
2020-07-06 14:21:50 +0100

    First version of login page

---
+++ pages/login.tsx
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+import Head from 'next/head';
+import NavBar from '../components/navbar';
+import Link from 'next/link';
+
+const Login = () => (
+  <>
+    <NavBar></NavBar>
+
+    <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+      <Head>
+        <title>Dev Team Up</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+      </Head>
+
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image size="mini" src="/lore-ipsum-logo.jpg" /> Log in to your
+          account
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+
+            <Button color="teal" fluid size="large">
+              Log In
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New here?{' '}
+          <Link href="/signup">
+            <a>Sign Up</a>
+          </Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </>
+);
+
+export default Login;
